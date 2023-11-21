266f670a184a063c4ac421e1108e2456536acf12
Kyndallmae <kyndallmae@gmail.com>
2023-11-21 16:35:17 -0600

    modified connection

--- Develop/server/config/connection.js
+++ Develop/server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI ||'mongodb+srv://Carothers:Archie0702!@cluster0.7eyvk8w.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect(process.env.MONGODB_URI ||'mongodb+srv://booksearchbonanza:Archie0702!@cluster0.7eyvk8w.mongodb.net/?retryWrites=true&w=majority');
 
 module.exports = mongoose.connection;
