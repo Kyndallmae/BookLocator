47171c93408cfdb9ee0b8124a63e8315acc6d7c6
Kyndallmae <kyndallmae@gmail.com>
2023-11-21 16:27:55 -0600

    added mongoddb connection

--- Develop/server/config/connection.js
+++ Develop/server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI ||'mongodb+srv://Carothers:Archie0702!@cluster0.7eyvk8w.mongodb.net/?retryWrites=true&w=majority');
 
 module.exports = mongoose.connection;
