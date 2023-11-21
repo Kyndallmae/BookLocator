8c391260fbe0d0727911db63dde9ee40e197eca6
Kyndallmae <kyndallmae@gmail.com>
2023-11-21 15:44:59 -0600

    modfied savedbooks

--- Develop/client/src/pages/SavedBooks.jsx
+++ Develop/client/src/pages/SavedBooks.jsx
@@ -1,70 +1,57 @@
 import { useState, useEffect } from 'react';
 import {
   Container,
-  Col,
-  Form,
-  Button,
   Card,
-  Row
+  Button,
+  Row,
+  Col
 } from 'react-bootstrap';
 
+import { useQuery, useMutation } from "@apollo/client";
+import { QUERY_ME } from "../utils/queries";
+import { REMOVE_BOOK } from "../utils/mutations";
+
+import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
-import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { removeBookId } from '../utils/localStorage';
 
-const SearchBooks = () => {
-  // Create state for holding returned google api data
-  const [searchedBooks, setSearchedBooks] = useState([]);
-  // Create state for holding our search field data
-  const [searchInput, setSearchInput] = useState('');
+const SavedBooks = () => {
+  const [userData, setUserData] = useState({});
+  const { loading, data } = useQuery(QUERY_ME);
+  const [removeBook, { error }] = useMutation(REMOVE_BOOK);
 
-  // Create state to hold saved bookId values
-  const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
-  // Set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
-  // Learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
+
+  // use this to determine if `useEffect()` hook needs to run again
+  const userDataLength = Object.keys(userData).length;
+
   useEffect(() => {
-    return () => saveBookIds(savedBookIds);
-  });
+    const getUserData = async () => {
+      try {
+        const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-  // Create method to search for books and set state on form submit
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
+        if (!token) {
+          return false;
+        }
 
-    if (!searchInput) {
-      return false;
-    }
+        const response = await getMe(token);
 
-    try {
-      const response = await searchGoogleBooks(searchInput);
+        if (!response.ok) {
+          throw new Error('something went wrong!');
+        }
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
+        const user = await response.json();
+        setUserData(user);
+      } catch (err) {
+        console.error(err);
       }
+    };
 
-      const { items } = await response.json();
+    getUserData();
+  }, [userDataLength]);
 
-      const bookData = items.map((book) => ({
-        bookId: book.id,
-        authors: book.volumeInfo.authors || ['No author to display'],
-        title: book.volumeInfo.title,
-        description: book.volumeInfo.description,
-        image: book.volumeInfo.imageLinks?.thumbnail || '',
-      }));
-
-      setSearchedBooks(bookData);
-      setSearchInput('');
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  // Create function to handle saving a book to our database
-  const handleSaveBook = async (bookId) => {
-    // Find the book in `searchedBooks` state by the matching id
-    const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
-
-    // Get token
+  // create function that accepts the book's mongo _id value as param and deletes the book from the database
+  const handleDeleteBook = async (bookId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
     if (!token) {
@@ -72,74 +59,52 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const response = await deleteBook(bookId, token);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
       }
 
-      // If book successfully saves to user's account, save book id to state
-      setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+      const updatedUser = await response.json();
+      setUserData(updatedUser);
+      // upon success, remove book's id from localStorage
+      removeBookId(bookId);
     } catch (err) {
       console.error(err);
     }
   };
 
+  // if data isn't here yet, say so
+  if (!userDataLength) {
+    return <h2>LOADING...</h2>;
+  }
+
   return (
     <>
-      <div className="text-light bg-dark p-5">
+      <div fluid className="text-light bg-dark p-5">
         <Container>
-          <h1>Search for Books!</h1>
-          <Form onSubmit={handleFormSubmit}>
-            <Row>
-              <Col xs={12} md={8}>
-                <Form.Control
-                  name='searchInput'
-                  value={searchInput}
-                  onChange={(e) => setSearchInput(e.target.value)}
-                  type='text'
-                  size='lg'
-                  placeholder='Search for a book'
-                />
-              </Col>
-              <Col xs={12} md={4}>
-                <Button type='submit' variant='success' size='lg'>
-                  Submit Search
-                </Button>
-              </Col>
-            </Row>
-          </Form>
+          <h1>Viewing saved books!</h1>
         </Container>
       </div>
-
       <Container>
         <h2 className='pt-5'>
-          {searchedBooks.length
-            ? `Viewing ${searchedBooks.length} results:`
-            : 'Search for a book to begin'}
+          {userData.savedBooks.length
+            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
+            : 'You have no saved books!'}
         </h2>
         <Row>
-          {searchedBooks.map((book) => {
+          {userData.savedBooks.map((book) => {
             return (
-              <Col md="4" key={book.bookId}>
-                <Card border='dark'>
-                  {book.image ? (
-                    <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' />
-                  ) : null}
+              <Col md="4">
+                <Card key={book.bookId} border='dark'>
+                  {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
                   <Card.Body>
                     <Card.Title>{book.title}</Card.Title>
                     <p className='small'>Authors: {book.authors}</p>
                     <Card.Text>{book.description}</Card.Text>
-                    {Auth.loggedIn() && (
-                      <Button
-                        disabled={savedBookIds?.some((savedBookId) => savedBookId === book.bookId)}
-                        className='btn-block btn-info'
-                        onClick={() => handleSaveBook(book.bookId)}>
-                        {savedBookIds?.some((savedBookId) => savedBookId === book.bookId)
-                          ? 'This book has already been saved!'
-                          : 'Save this Book!'}
-                      </Button>
-                    )}
+                    <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
+                      Delete this Book!
+                    </Button>
                   </Card.Body>
                 </Card>
               </Col>
@@ -151,4 +116,4 @@ const SearchBooks = () => {
   );
 };
 
-export default SearchBooks;
+export default SavedBooks;
